b58b5e770a693690d07b070057d83e204ca149a5
smyye <smyyeoztrk43@gmail.com>
2024-04-17 21:09:54 +0300

    sync

--- src/models/category.js
+++ src/models/category.js
@@ -1,6 +1,20 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true, collection: "categories" }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
